4f73d4c9978ae2a890130e417efb908e90de8640
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-01-03 14:12:19 +0100

    Strongly-typed slide

--- FP-at-Teufel.playground/Pages/12 - Strongly-typed.xcplaygroundpage/Contents.swift
+++ FP-at-Teufel.playground/Pages/12 - Strongly-typed.xcplaygroundpage/Contents.swift
@@ -3,9 +3,43 @@
 
  ### (Very) Strongly-typed
 
- VERY strongly-typed, let the compiler work for you and test for you. Deep knowledge about Generics is what we recommend. When you have possible inheritance or protocol inheritance and you know that all possible implementations are a closed list that's never (or rarely) expected to change, prefer enums over inheritance, and NEVER user "default". Let the compiler warn you when you introduce a new case.. Finally, protocols with associated types can bring some problems, in several situations they can be replaced by a struct that, in the initialiser has all the behaviour injected as closures. This is called protocol witness and Swift compiler does that for you during compilation, but if you do it yourself, you unlock some new possibilities and avoid type-erasure. One warning here, this part may make your build slower, this is a very honest reality. In that case, avoiding type-inference, specially in closures with generics can save the day. So please be careful, Swift is improving this a lot recently but there are still some hiccups.
- */
+ Why?
+ - Compile-time safety
+ - More code reuse
+ - Better refactoring
+ - Fewer and better tests
 
+ How?
+ - Learning in depth Swift Generics and protocol associated types, their limitations and roadmap
+ - Understanding type-erasure, protocol witness and constraints
+ - Using enums instead of protocols/inheritance
+ - - -
+*/
+struct Cat { }
+struct Dog { }
+
+// Instead of
+protocol Animal_ { }
+extension Cat: Animal_ { }
+extension Dog: Animal_ { }
+
+func pet(animal: Animal_) {
+    if animal is Cat { }
+    if animal is Dog { }
+}
+
+// Use enums
+enum Animal {
+    case dog(Dog)
+    case cat(Cat)
+}
+
+func pet(animal: Animal) {
+    switch animal {
+    case let .cat(cat): notImplemented()
+    case let .dog(dog): notImplemented()
+    }
+}
 /*:
  [Previous](@previous) | [Topics](04%20-%20Topics) | [Next](@next)
  */
