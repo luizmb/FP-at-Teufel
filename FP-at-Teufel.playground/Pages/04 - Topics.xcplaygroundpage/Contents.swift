9d2975038f477178904319ee1bbdcb5721cdcc5a
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-01-03 12:18:05 +0100

    Add FRP topic

--- FP-at-Teufel.playground/Pages/04 - Topics.xcplaygroundpage/Contents.swift
+++ FP-at-Teufel.playground/Pages/04 - Topics.xcplaygroundpage/Contents.swift
@@ -10,10 +10,11 @@
  * [Lazyness and Explicitness](08%20-%20Lazyness%20and%20Explicitness)
  * [Declarative syntax](09%20-%20Declarative%20Syntax)
  * [Focus on Data Structures](10%20-%20Focus%20on%20Data%20Structures)
- * [(Very) Strongly-typed](11%20-%20Strongly-typed)
- * [Tagged](12%20-%20Tagged)
- * [Optics](13%20-%20Optics)
- * [Algebraic Data Types](14%20-%20Algebraic%20Data%20Types)
+ * [FRP](11%20-%20FRP)
+ * [(Very) Strongly-typed](12%20-%20Strongly-typed)
+ * [Tagged](13%20-%20Tagged)
+ * [Optics](14%20-%20Optics)
+ * [Algebraic Data Types](15%20-%20Algebraic%20Data%20Types)
 
  [Previous](@previous) | [Next](@next)
  */
---
+++ FP-at-Teufel.playground/Pages/11 - FRP.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+/*:
+ [Previous](@previous) | [Topics](04%20-%20Topics) | [Next](@next)
+
+ ### Functional Reactive Programming
+
+ */
+
+/*:
+ [Previous](@previous) | [Topics](04%20-%20Topics) | [Next](@next)
+ */
